b8bc49ab5f610beed4046a0f3881e1232b58001f
Mark McDonnell <mark.mcdx@gmail.com>
2013-03-24 20:17:25 +0000

    Initial files - practically all work done by @danscotton

---
+++ sass.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#sass --style expanded --watch application.scss:application.css --debug-info
+#sass --style compressed --watch application.scss:application.css
+
+sass --style expanded --watch application.scss:application.css --debug-info
+
+exit 0
